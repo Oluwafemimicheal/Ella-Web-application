bf6b394ec9c445ca0e3c06920a6961ec439b109e
oluwafemi michael <femi4410@gmail.com>
2026-03-30 21:50:46 +0100

    Correcting the sort UI display

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,19 +33,46 @@ const Home = () => {
 export const SortAndFilterUI = ({ closeSort }) => {
   return (
     <div className='fixed w-full h-screen bg-black/50 drop-shadow-md z-99 top-0 overflow-hidden'>
-      <div className='absolute bottom-0 w-full bg-white rounded-t-3xl h-[40%]'>
-        <div className='flex justify-between items-center py-5 px-3 border-b border-gray-300'>
-          <small className='text-lg'>Sort by</small>
+      <div className='absolute bottom-0 w-full bg-white rounded-t-3xl h-auto'>
+        <div className='w-full flex justify-center items-center'>
+          <div className=' w-20 h-2 mt-2 bg-gold rounded-full'></div>
+        </div>
+        <div className='flex justify-between items-center py-4 px-3 border-b border-gray-300'>
+          <small className='text-lg font-semibold'>Sort by</small>
           <button onClick={() => closeSort(false)}><Plus className='rotate-45' /></button>
         </div>
 
-        <ul className='flex flex-col gap-y-5 px-3 mt-10'>
-          <li>Popularity</li>
-          <li>Newest Arrivals</li>
-          <li>Price: Low to High</li>
-          <li>Price: High to Low</li>
-          <li>Customer Rating</li>
-        </ul>
+
+        <form className="py-5 px-3 flex flex-col gap-5">
+          {["Popularity", "Newest Arrivals", "Price: Low to High", "Price: High to Low", "Customer Rating"].map((option) => (
+            <label
+              key={option}
+              className="flex justify-between items-center gap-3 cursor-pointer group"
+            >
+              <span className="text-gray-900 group-hover:text-black transition">
+                {option}
+              </span>
+              {/* Hidden native radio */}
+              <input
+                type="radio"
+                name="filter"
+                value={option}
+                className="hidden peer"
+              />
+
+              {/* Custom radio */}
+              <div className="w-5 h-5  flex items-center justify-center rounded-full border-2 border-gray-400 
+                      peer-checked:border-gold peer-checked:bg-gold
+                      transition-all duration-200">
+                <div className="w-2.5 h-2.5 p-3 bg-white rounded-full scale-0 
+                        peer-checked:scale-100 transition-transform duration-200" />
+              </div>
+
+              {/* Label text */}
+
+            </label>
+          ))}
+        </form>
       </div>
     </div>
   )
